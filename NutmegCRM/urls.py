ff622c0f26151909ef9c7d8ffd0ccf6808fa43a9
Justin Crawford <Justasic@Gmail.com>
2015-03-04 23:37:14 -0800

    Progress. I need to sleep now. At least we can get to Django Admin easily.

--- NutmegCRM/urls.py
+++ NutmegCRM/urls.py
@@ -9,8 +9,6 @@ from django.contrib.auth import views as authviews
 admin.autodiscover()
 
 urlpatterns = [
-    # Instant search
-    #url(r'^ajax_search/',include('ajax_search.urls')),
     # Index for the entire site
     url(r'^$', 'NutmegCRM.apps.overview.views.index', name='home'),
 
